48a87c24304205e433b0b3e84e41035defc926b8
Dawnwin7 <jackson.zhang0429@gmail.com>
2026-06-10 11:39:55 +0800

    feat(web): site metadata

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,15 +1,39 @@
-import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google"
+import { Geist, JetBrains_Mono } from "next/font/google"
 
 import "@workspace/ui/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@workspace/ui/lib/utils";
+import { cn } from "@workspace/ui/lib/utils"
+import { Metadata } from "next"
 
 const fontSans = Geist({
   subsets: ["latin"],
   variable: "--font-sans",
 })
 
-const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'})
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+export const metadata: Metadata = {
+  title: "Chance Journey",
+  description: "CJ changes the world.",
+  icons: {
+    icon: [
+      {
+        url: "/brand/favicon.ico",
+      },
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/brand/favicon-light.svg",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/brand/favicon-dark.svg",
+      },
+    ],
+  },
+}
 
 export default function RootLayout({
   children,
@@ -20,7 +44,12 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontSans.variable, "font-mono", jetbrainsMono.variable)}
+      className={cn(
+        "antialiased",
+        fontSans.variable,
+        "font-mono",
+        jetbrainsMono.variable
+      )}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
